a00900734303146f1e453e37f751bbfcfaade3c2
NandoSchlemper <bkschlemper@gmail.com>
2024-09-04 08:52:00 -0300

    bruh

--- backend/cmd/main.js
+++ backend/cmd/main.js
@@ -1,8 +1,8 @@
-import {Connect2DB, db} from "../database/db.js";
+import {Connect2DB} from "../database/db.js";
 import {ServerConnection} from "./api/api.js";
 
 async function main() { 
     await ServerConnection()
-    await Connect2DB(db)
+    await Connect2DB()
 }
 await main()

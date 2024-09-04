5b12fa8d717bfb579cb68301611a6dcab96aa78c
NandoSchlemper <bkschlemper@gmail.com>
2024-09-04 09:28:38 -0300

    Configurando setup para MongoDB, criando Branch 'beckdoend'

--- backend/cmd/main.js
+++ backend/cmd/main.js
@@ -1,8 +1,6 @@
-import {Connect2DB} from "../database/db.js";
 import {ServerConnection} from "./api/api.js";
 
 async function main() { 
     await ServerConnection()
-    await Connect2DB()
 }
 await main()

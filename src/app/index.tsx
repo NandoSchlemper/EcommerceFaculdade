ab49842dcd0f9a23442f1287b2cee8c01426f9b4
Henrique Stocco <henriquestoccomachado@gmail.com>
2024-09-08 11:21:28 -0300

    feat: add register screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,9 @@
 // import Home from "../pages/Home";
-import Login from "../pages/Login";
-
+import Register from "../pages/Register";
 import "../styles/App.css";
 
 function App() {
-  return <Login />;
+  return <Register />;
 }
 
 export default App;
